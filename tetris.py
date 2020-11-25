bf5d3fe822b862aa350f1d8ca729563e1f1befc8
SiafroD <74325090+SiafroD@users.noreply.github.com>
2020-11-25 09:31:10 +0100

    Accélération de la descente finale

--- tetris.py
+++ tetris.py
@@ -3,7 +3,7 @@ from tetris_class import *
 from random import randint
 
 controls = {
-    #'s' : (0,1), #accélération de la descente --> /!\ à initialiser
+    's' : (0,1), #accélération de la descente --> /!\ à initialiser
     'd' : 1, #mouvement à droite
     'q' : -1 #mouvement à gauche
     }
@@ -11,13 +11,16 @@ controls = {
 def mouvements(event):
     if event.char in controls:
         #bloc.direction = controls[event.char]
-        bloc.mouvement(controls[event.char])
-        w.delete(ALL)
-        for cell in bloc.corps:
-            xc,yc = cell
-            w.create_rectangle(xc,yc,xc+jeu.step,yc+jeu.step,fill="green", outline="")
+        if event.char == "s":
+            accelerer(event)
+        else:
+            bloc.mouvement(controls[event.char])
+            w.delete(ALL)
+            for cell in bloc.corps:
+                xc,yc = cell
+                w.create_rectangle(xc,yc,xc+jeu.step,yc+jeu.step,fill="green", outline="")
 
-def accélérer(event):
+def accelerer(event):
     if event.char == "s":
         if jeu.frame_rate >= 400:
             jeu.frame_rate = jeu.frame_rate // 2
@@ -49,9 +52,8 @@ bloc = Block([
     [(apparence[2][0]*jeu.step+jeu.coo_start[0]),(apparence[2][1]*jeu.step+jeu.coo_start[1])],
     [(apparence[3][0]*jeu.step+jeu.coo_start[0]),(apparence[3][1]*jeu.step+jeu.coo_start[1])]],jeu)
 #==================================
-root.bind("<Key>", mouvements)
-root.bind("<KeyPress>", accélérer)
 root.bind("<KeyRelease>", ralentir)
+root.bind("<Key>", mouvements)
 w = Canvas(root, width=jeu.largeur, height=jeu.hauteur)
 v = StringVar()
 v.set(f"{jeu.frame_rate}")
